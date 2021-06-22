94b9dd2ba60e06dbd49b71ffe42ce65c60cd6011
Awlas <salwa.fatini@gmail.com>
2021-06-22 19:40:44 +0200

    Conditions detection

--- test_script_rotation.js
+++ test_script_rotation.js
@@ -25,6 +25,7 @@ function matrix_vec_mult(mat, vec) {
   return result;
 }
 
+// Fonction qui dessine une ligne
 function drawLine(start, end, ctx) {
   ctx.beginPath();
   ctx.moveTo(start[0], start[1]);
@@ -98,31 +99,57 @@ function tri_fonction(topcodes, hauteur_case, haut_droite, haut_gauche)
   tableau_points = [[], [], [], []];
   topcodes.forEach(e => {
     let num_fonc;
-    if(isvertices(e) || e.y < haut_gauche.y || e.y < haut_droite.y)
+    if(isvertices(e) || (e.y + 10) < haut_gauche.y || (e.y + 10) < haut_droite.y)
     {
       num_fonc = -1;
     }
     else
     {
-      num_fonc = Math.floor(e.y/hauteur_case);
+      if(e.code >= 103 && e.code <= 143){
+        num_fonc = Math.floor(e.y/hauteur_case);
+        switch (num_fonc) {
+          case 0:
+            tableau_points[0].push(e);
+            break;
+          case 1:
+            tableau_points[1].push(e);
+            break;
+          case 2:
+            tableau_points[2].push(e);
+            break;
+          case 3:
+            tableau_points[3].push(e);
+            break;
+          default:
+            break;
+        }
+      }
+      else{
+        if(e.code >= 157 && e.code <= 181)
+        {
+          num_fonc = Math.floor((e.y - (2 * hauteur_case / 3))/hauteur_case) + 1;
+          console.log(num_fonc);
+          switch (num_fonc) {
+            case 0:
+              tableau_points[0].push(e);
+              break;
+            case 1:
+              tableau_points[1].push(e);
+              break;
+            case 2:
+              tableau_points[2].push(e);
+              break;
+            case 3:
+              tableau_points[3].push(e);
+              break;
+            default:
+              break;
+          }
+        }
+      }
     }
 
-    switch (num_fonc) {
-      case 0:
-        tableau_points[0].push(e);
-        break;
-      case 1:
-        tableau_points[1].push(e);
-        break;
-      case 2:
-        tableau_points[2].push(e);
-        break;
-      case 3:
-        tableau_points[3].push(e);
-        break;
-      default:
-        break;
-    }
+    
   });
 }
 
@@ -140,78 +167,64 @@ TopCodes.setVideoFrameCallback("video-canvas", function (jsonString) {
   for (i = 0; i < topcodes.length; i++) {      
     ctx.font = '24px serif';
     ctx.fillText(topcodes[i].code.toString(), topcodes[i].x, topcodes[i].y + 50);
-    ctx.strokeRect(200, 60, 900, 600);
+    // ctx.strokeRect(200, 60, 900, 600);
   };
 
 
-  document.getElementById("scanButton").onclick = function(){
-    scan(json.topcodes);  
-    TopCodes.stopVideoScan('video-canvas');
-    document.getElementById("modal").style.display = "none";
+  document.getElementById("scan-button").onclick = function () {
+    sessionStorage.setItem('scan', jsonString);
+
+    let haut_droite;
+    let haut_gauche;
+    let bas_droite;
+    let bas_gauche;
+
+    for (let i = 0; i < topcodes.length; i++) {
+      switch (topcodes[i].code) {
+        case 55:
+          haut_gauche = topcodes[i];
+          break;
+        case 59:
+          haut_droite = topcodes[i];
+          break;
+        case 47:
+          bas_gauche = topcodes[i];
+          break;
+        case 61:
+          bas_droite = topcodes[i];
+          break;
+        default:
+          break;
+      }
+    }
+
+    if (haut_droite === undefined || haut_gauche === undefined || bas_gauche === undefined || bas_droite === undefined) {
+      alert("Les quatres coins n'ont pas été détecté");
+      return;
+    }
+
+    translation(topcodes, haut_droite);
+    rotation_matrix(topcodes, bas_droite, bas_gauche);
+
+    drawLine([haut_droite.x, haut_droite.y], [bas_droite.x, bas_droite.y], ctx);
+    drawLine([bas_droite.x, bas_droite.y], [bas_gauche.x, bas_gauche.y], ctx);
+    drawLine([bas_gauche.x, bas_gauche.y], [haut_gauche.x, haut_gauche.y], ctx);
+    drawLine([haut_gauche.x, haut_gauche.y],[haut_droite.x, haut_droite.y], ctx);
+
+    let hauteur = bas_gauche.y - haut_gauche.y;
+    let largeur = haut_gauche.x - haut_droite.x;
+    let hauteur_case = (hauteur/4);
+    let largeur_case = (largeur/9);
+
+    tri_fonction(topcodes, hauteur_case, haut_droite, haut_gauche);
+
+    tableau_instruc = [];
+
+    index(tableau_points, tableau_instruc, haut_droite, haut_gauche, largeur_case);
+
+    console.log(tableau_instruc);
   }
 });
-
-
-function scan(topcodes) {
-  let haut_droite;
-  let haut_gauche;
-  let bas_droite;
-  let bas_gauche;
-
-  for (let i = 0; i < topcodes.length; i++) {
-    switch (topcodes[i].code) {
-      case 55:
-        haut_gauche = topcodes[i];
-        break;
-      case 59:
-        haut_droite = topcodes[i];
-        break;
-      case 47:
-        bas_gauche = topcodes[i];
-        break;
-      case 61:
-        bas_droite = topcodes[i];
-        break;
-      default:
-        break;
-    }
-  }
-
-  translation(topcodes, haut_droite);
-  rotation_matrix(topcodes, bas_droite, bas_gauche);
-
-  drawLine([haut_droite.x, haut_droite.y], [bas_droite.x, bas_droite.y], ctx);
-  drawLine([bas_droite.x, bas_droite.y], [bas_gauche.x, bas_gauche.y], ctx);
-  drawLine([bas_gauche.x, bas_gauche.y], [haut_gauche.x, haut_gauche.y], ctx);
-  drawLine([haut_gauche.x, haut_gauche.y],[haut_droite.x, haut_droite.y], ctx);
-
-  let hauteur = bas_gauche.y - haut_gauche.y;
-  let largeur = haut_gauche.x - haut_droite.x;
-  let hauteur_case = (hauteur/4);
-  let largeur_case = (largeur/9);
-
-  tri_fonction(topcodes, hauteur_case, haut_droite, haut_gauche);
-
-  tableau_instruc = [[0]];
-
-  index(tableau_points, tableau_instruc, haut_droite, haut_gauche, largeur_case);
-
-  var difficulty = 0;
-  var level = 0;
-
-  tableau_instruc.forEach(e => {
-    if (e[1] == 3 && e[2] == 6) {
-      difficulty = difficultySelect(e[0]);
-    };
-    if (e[1] == 3 && e[2] == 7) {
-      level = levelSelect(e[0]);
-    };
-  });
-  tableau_instruc[0][0] = difficulty*6+level;
-
-  send(tableau_instruc);
-}
-
 
 var topcodes = [];
 
@@ -219,42 +232,3 @@ var ctx = document.querySelector("#video-canvas").getContext('2d');
 
 var tableau_instruc = [];
 var tableau_points = [];
-
-function send(tab) {
-	console.log(tab);
-	var array = tab;
-	var hash = btoa(JSON.stringify(array));
-	console.log(hash);
-	var frame = document.getElementById("frame");
-	frame.src="cargo-not/index.html#"+hash;
-}
-
-function difficultySelect(code) {
-  switch (code) {
-    case 205:
-      return 0;
-    case 221:
-      return 1;
-    default:
-      return 0;
-  }
-}
-
-function levelSelect(code) {
-  switch (code) {
-    case 279:
-      return 1;
-    case 283:
-      return 2;
-    case 285:
-      return 3;
-    case 295:
-      return 4;
-    case 299:
-      return 5;
-    case 301:
-      return 6;
-    default:
-      return 0;
-  }
-}

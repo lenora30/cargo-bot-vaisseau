41b00d0ea3ec1006020cccd3575c2366759cb106
Awlas <86008304+Awlas@users.noreply.github.com>
2021-07-05 11:59:50 +0000

    Deploying to gh-pages from @ Pinguee/cargo-not@be14a6478ef5293e315e6feefcbe2beee16d4166 🚀

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 
 $(window).scroll(function() {
 
-    var posScroll = $(this).scrollTop() / 20;
+    var posScroll = $(this).scrollTop() / 70;
 
     $("#debut").css({
         "background-size" : 100 + posScroll + "%"

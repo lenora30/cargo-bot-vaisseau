14089986742e8b00759717a4b7062ea2f1367b73
Awlas <salwa.fatini@gmail.com>
2021-07-07 13:36:03 +0200

    Add constants

---
+++ scripts/constants.js
@@ -0,0 +1,48 @@
+const DIFFICULTY = { 
+    "205": 0,
+    "211": 1,
+    "213": 2,
+    "217": 3,
+    "227": 4,
+    "229": 5,
+    "233": 6,
+    "241": 7,
+    "271": 8
+};
+
+const LEVEL = {
+    "279": 1,
+    "283": 2,
+    "285": 3,
+    "295": 4,
+    "299": 5,
+    "301": 6
+};
+
+const CODE_MIN_COMMAND = 103;
+
+const CODE_MAX_COMMAND = 143;
+
+const CODE_MIN_CONDITION = 157;
+
+const CODE_MAX_CONDITION = 181;
+
+const CODE_MIN_DIFFICULTY = 205;
+
+const CORNER = {
+    top_left: 55,
+    top_right: 59,
+    bottom_left: 47,
+    bottom_rigth: 61
+};
+
+const DST_CORNER  = {
+    top_left_x : 0,
+    top_left_y : 0,
+    top_right_x : 1200,
+    top_right_y : 0,
+    bottom_right_x : 1200,
+    bottom_right_y : 800,
+    bottom_left_x : 0,
+    bottom_left_y : 800
+};

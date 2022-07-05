7fef9edbc739bb92068da862a4e337d9c1d994f5
ledac.enora <ledac.enora@etu.unistra.fr>
2022-07-05 14:36:45 +0200

    AIde

--- script_style.js
+++ script_style.js
@@ -1,12 +1,20 @@
 var close = document.getElementsByClassName("close");
 var title = document.getElementsByClassName("title");
 var regles = document.getElementById("regles");
+var bouton = document.getElementById("problems");
+console.log(bouton.nextSibling.nextSibling);
 
 for (let i = 0; i < title.length; i++) {
     title[i].onclick = function () {
         this.parentElement.classList.toggle("close");
         this.parentElement.classList.toggle("open");
     };
+}
+
+bouton.onclick = function () {
+    
+    this.nextSibling.nextSibling.classList.toggle("close");
+    this.nextSibling.nextSibling.classList.toggle("open");
 }
 
 
